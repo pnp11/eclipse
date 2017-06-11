86e79291fca7624a141ac968d19b801f062fd91c
Piotr Nowak X <piotr.x.nowak@ericsson.com>
2017-06-11 17:24:20 +0200

    Added new comment to class Constants2

--- JAVA_exercise/src/Constants2.java
+++ JAVA_exercise/src/Constants2.java
@@ -1,6 +1,11 @@
 
 public class Constants2 {
-	public static final double CM_PER_INCH = 2.54;
+	/**
+	 * Definicja stalej klasowej na zewnatrzmetody main. W zwiazku z tym stala ta moze byc uzytwana 
+	 * takze przez inne metody tej klasy. Ponadto stala jest zadeklarowana jako publiczna,
+	 * dostep do niej maja takze metody innej klasy - jak w naszym przykladzie Constants2.CM_PER_INCH.
+	 */
+	public static final double CM_PER_INCH = 2.54; 
 	
 	public static void main(String[] args)
 	{
